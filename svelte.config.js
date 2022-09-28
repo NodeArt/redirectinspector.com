a0297e229f4f08dca72878c59dcaaeb725759ea1
Vasiliy Manilo <v.manilo@nodeart.io>
2022-09-28 10:44:42 +0300

    feat(adapter): add adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,14 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
 	}
 };
 
